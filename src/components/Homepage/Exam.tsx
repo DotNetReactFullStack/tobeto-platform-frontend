d0e978cc10579b47070a05813cc28b87e0cc214a
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-07 20:14:56 +0300

    Updated Exam component

--- src/components/Homepage/Exam.tsx
+++ src/components/Homepage/Exam.tsx
@@ -1,38 +1,37 @@
 import React from "react";
 import "./Exam.css";
 import ExamDetailModal from "./ExamDetailModal";
+import ShowMoreButton from "../ShowMoreButton/ShowMoreButton";
 
 type Props = {};
 
 const Exam = (props: Props) => {
   return (
-    <div className="container main-section exam-component-margin">
+    <div className="container main-section">
       <div className="exam-component">
         <div className="exam-component-header">
           <span className="exam-component-header-title">Sınavlarım</span>
         </div>
-        <div className="exam-elements">
-          <button
-            className="exam-element"
-            type="button"
+        <div className="exam-component-body">
+
+          <div className="exam-element"
             data-bs-toggle="modal"
-            data-bs-target="#staticBackdrop"
-          >
+            data-bs-target="#staticBackdrop">
             <div className="exam-element-header">
               <div className="exam-element-header-left-side">
-                <span className="exam-element-header-title">
+                <span className="exam-element-title">
                   Herkes için Kodlama 1B Değerlendirme Sınavı
                 </span>
               </div>
-              <div className="exam-header-right-side">
+              <div className="exam-element-header-right-side">
                 <img
                   className="image-exam-status"
-                  src={process.env.PUBLIC_URL + "/exam-status-done.png"}
+                  src={process.env.PUBLIC_URL + "/exam-status-completed-icon.png"}
                 />
               </div>
             </div>
             <div className="exam-element-content">
-              <span className="exam-element-content-title">
+              <span className="exam-element-subtitle">
                 Herkes İçin Kodlama - 1B
               </span>
             </div>
@@ -40,8 +39,92 @@ const Exam = (props: Props) => {
               <i className="bi bi-stopwatch exam-element-footer-icon"></i>
               <span className="exam-element-footer-duration">45 Dakika</span>
             </div>
-          </button>
+          </div>
+
+
+          <div className="exam-element"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            <div className="exam-element-header">
+              <div className="exam-element-header-left-side">
+                <span className="exam-element-title">
+                  Herkes için Kodlama 1B Değerlendirme Sınavı
+                </span>
+              </div>
+              <div className="exam-element-header-right-side">
+                <img
+                  className="image-exam-status"
+                  src={process.env.PUBLIC_URL + "/exam-status-completed-icon.png"}
+                />
+              </div>
+            </div>
+            <div className="exam-element-content">
+              <span className="exam-element-subtitle">
+                Herkes İçin Kodlama - 1B
+              </span>
+            </div>
+            <div className="exam-element-footer">
+              <i className="bi bi-stopwatch exam-element-footer-icon"></i>
+              <span className="exam-element-footer-duration">45 Dakika</span>
+            </div>
+          </div>
+
+          <div className="exam-element"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            <div className="exam-element-header">
+              <div className="exam-element-header-left-side">
+                <span className="exam-element-title">
+                  Herkes için Kodlama 1B Değerlendirme Sınavı
+                </span>
+              </div>
+              <div className="exam-element-header-right-side">
+                <img
+                  className="image-exam-status"
+                  src={process.env.PUBLIC_URL + "/exam-status-completed-icon.png"}
+                />
+              </div>
+            </div>
+            <div className="exam-element-content">
+              <span className="exam-element-subtitle">
+                Herkes İçin Kodlama - 1B
+              </span>
+            </div>
+            <div className="exam-element-footer">
+              <i className="bi bi-stopwatch exam-element-footer-icon"></i>
+              <span className="exam-element-footer-duration">45 Dakika</span>
+            </div>
+          </div>
+
+
+          <div className="exam-element"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            <div className="exam-element-header">
+              <div className="exam-element-header-left-side">
+                <span className="exam-element-title">
+                  Herkes için Kodlama 1B Değerlendirme Sınavı
+                </span>
+              </div>
+              <div className="exam-element-header-right-side">
+                <img
+                  className="image-exam-status"
+                  src={process.env.PUBLIC_URL + "/exam-status-completed-icon.png"}
+                />
+              </div>
+            </div>
+            <div className="exam-element-content">
+              <span className="exam-element-subtitle">
+                Herkes İçin Kodlama - 1B
+              </span>
+            </div>
+            <div className="exam-element-footer">
+              <i className="bi bi-stopwatch exam-element-footer-icon"></i>
+              <span className="exam-element-footer-duration">45 Dakika</span>
+            </div>
+          </div>
         </div>
+        <ShowMoreButton />
       </div>
       <ExamDetailModal />
     </div>
