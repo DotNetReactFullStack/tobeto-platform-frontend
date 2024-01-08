9ed6cc8bbda287e88ee9b360daa73a525cf3161f
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-08 23:37:13 +0300

    Refactor: Added RecourseElement component

--- src/components/Homepage/UserContentNavComponents/Recourse.tsx
+++ src/components/Homepage/UserContentNavComponents/Recourse.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../UserContentNavComponents/Recourse.css'
 import ShowMoreButton from '../../ShowMoreButton/ShowMoreButton'
+import RecourseElement from './RecourseElement'
 
 type Props = {}
 
@@ -10,85 +11,9 @@ const Recourse = (props: Props) => {
   return (
     <>
       <div className='recourse-component'>
-        <div className='recourse-element'>
-          <div className="recourse-header">
-
-            <div className="recourse-header-left-side">
-              <div className="recourse-organization-name number-of-lines-1 text-length-30ch">
-                İstanbul Kodluyor
-              </div>
-              <div className="recourse-type-info-title number-of-lines-1 text-length-30ch">
-                Bilgilendirme
-              </div>
-            </div>
-
-            <div className="recourse-header-right-side">
-              <div className="recourse-step number-of-lines-1 text-length-15ch">
-                Kabul Edildi
-              </div>
-            </div>
-
-          </div>
-          <div className="recourse-content">
-            <div className="recourse-detail-step number-of-lines-1 text-length-35ch">
-              <span className="recourse-detail-step-icon">
-                <i className="bi bi-check-lg"></i>
-              </span>
-              <span className="recourse-detail-step-description">
-                İstanbul Kodluyor Başvuru Formu onaylandı.
-              </span>
-            </div>
-            <div className="recourse-detail-step number-of-lines-1 text-length-35ch">
-              <span className="recourse-detail-step-icon">
-                <i className="bi bi-check-lg"></i>
-              </span>
-              <span className="recourse-detail-step-description">
-                İstanbul Kodluyor Belge Yükleme Formu onaylandı.
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div className='recourse-element'>
-          <div className="recourse-header">
-
-            <div className="recourse-header-left-side">
-              <div className="recourse-organization-name number-of-lines-1 text-length-30ch">
-                İstanbul Kodluyor
-              </div>
-              <div className="recourse-type-info-title number-of-lines-1 text-length-30ch">
-                Bilgilendirme
-              </div>
-            </div>
-
-            <div className="recourse-header-right-side">
-              <div className="recourse-step number-of-lines-1 text-length-15ch">
-                Kabul Edildi
-              </div>
-            </div>
-
-          </div>
-          <div className="recourse-content">
-            <div className="recourse-detail-step number-of-lines-1 text-length-35ch">
-              <span className="recourse-detail-step-icon">
-                <i className="bi bi-check-lg"></i>
-              </span>
-              <span className="recourse-detail-step-description">
-                İstanbul Kodluyor Başvuru Formu onaylandı.
-              </span>
-            </div>
-            <div className="recourse-detail-step number-of-lines-1 text-length-35ch">
-              <span className="recourse-detail-step-icon">
-                <i className="bi bi-check-lg"></i>
-              </span>
-              <span className="recourse-detail-step-description">
-                İstanbul Kodluyor Belge Yükleme Formu onaylandı.
-              </span>
-            </div>
-          </div>
-        </div>
+        <RecourseElement />
+        <RecourseElement />
       </div>
-
       <ShowMoreButton />
     </>
   )
