c0b5fd9fc6150d9ed26d6edf2062382d71c4635e
OzgurSonmez <ozgursonmez00@gmail.com>
2024-01-09 13:49:10 +0300

    Refactor: Added NavCardElement component

--- src/components/Homepage/NavCard.tsx
+++ src/components/Homepage/NavCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavCard.css";
+import NavCardElement from "./NavCardElement";
 
 type Props = {};
 
@@ -7,42 +8,9 @@ const NavCard = (props: Props) => {
   return (
     <div className="container main-section">
       <div className="nav-card-component">
-
-        <div className="nav-card-element">
-          <div></div>
-          <div className="nav-card-element-body">
-            <div className="nav-card-element-title">Profilini Oluştur</div>
-          </div>
-          <div className="nav-card-element-footer">
-            <a href="#" className="nav-card-element-footer-link">
-              Başla
-            </a>
-          </div>
-        </div>
-
-        <div className="nav-card-element">
-          <div></div>
-          <div className="nav-card-element-body">
-            <span className="nav-card-element-title">Kendini Değerlendir</span>
-          </div>
-          <div className="nav-card-element-footer">
-            <a href="#" className="nav-card-element-footer-link">
-              Başla
-            </a>
-          </div>
-        </div>
-
-        <div className="nav-card-element">
-          <div></div>
-          <div className="nav-card-element-body">
-            <span className="nav-card-element-title">Öğrenmeye Başla</span>
-          </div>
-          <div className="nav-card-element-footer">
-            <a href="#" className="nav-card-element-footer-link">
-              Başla
-            </a>
-          </div>
-        </div>
+        <NavCardElement />
+        <NavCardElement />
+        <NavCardElement />
       </div>
     </div>
   );
