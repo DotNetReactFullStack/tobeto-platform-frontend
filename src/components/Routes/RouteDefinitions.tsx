3f7723eb04b60c56971ce74cebdf8155be57e6d2
OzgurSonmez <ozgursonmez00@gmail.com>
2024-01-09 21:55:22 +0300

    Added Announcements page

--- src/components/Routes/RouteDefinitions.tsx
+++ src/components/Routes/RouteDefinitions.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../../pages/Homepage/Homepage";
 import NotFound from "../../pages/NotFound/NotFound";
-import Recourse from "../Recourse/Recourse";
+import Announcements from "../../pages/Announcements/Announcements";
 
 type Props = {};
 
@@ -11,6 +11,7 @@ const RouteDefinitions = (props: Props) => {
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="*" element={<NotFound />} />
+      <Route path="/duyurular" element={<Announcements />} />
     </Routes>
   );
 };
