22237b1eb1bfeb49a4bf7ce4374b7c5ed60cd966
OzgurSonmez <ozgursonmez00@gmail.com>
2024-01-19 11:07:58 +0300

    Fix a small bug for the SocialMediaAccountElement

--- src/components/Profile/SocialMediaAccounts/SocialMediaAccountElement.tsx
+++ src/components/Profile/SocialMediaAccounts/SocialMediaAccountElement.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './SocialMediaAccountElement.css'
+import React from "react";
+import "./SocialMediaAccountElement.css";
 
 type Props = {
-    iconClass: string;
-    iconColor: string;
-    link: string;
-}
+  iconClass: string;
+  iconColor: string;
+  link: string;
+};
 
 const SocialMediaAccountElement = ({ iconClass, iconColor, link }: Props) => {
-    return (
-        <div className="social-media-account-element">
-            <div className="account-element-icon">
-                <a href={link} target='_blank'>
-                    <i className={iconClass} style={{ color: iconColor }}></i>
-                </a>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="profile-social-media-account-element">
+      <div className="account-element-icon">
+        <a href={link} target="_blank">
+          <i className={iconClass} style={{ color: iconColor }}></i>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default SocialMediaAccountElement
+export default SocialMediaAccountElement;
