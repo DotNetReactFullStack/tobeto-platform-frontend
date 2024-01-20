666eb7dba3cf9f78285cabf60bd5776a233240e6
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-20 17:18:29 +0300

    Updated ForeignLanguageForm component using InputContainer component

--- src/components/EditProfile/ForeignLanguage/ForeignLanguageForm.tsx
+++ src/components/EditProfile/ForeignLanguage/ForeignLanguageForm.tsx
@@ -1,30 +1,91 @@
 import React from "react";
 import "./ForeignLanguageForm.css";
+import InputContainer from "../InputContainer";
 
 type Props = {};
+
+const ifVisibilityIsTrue = (value: any) => value.visibility === true;
+let languagesOptionDataFilters = [ifVisibilityIsTrue];
+let languageLevelsOptionDataFilters = [ifVisibilityIsTrue];
+const sortByPriorityDesc = (a: any, b: any) => b.priority - a.priority;
+
+const languages = [
+  {
+    id: '1',
+    name: 'İngilizce',
+    priority: 4,
+    visibility: true
+  },
+  {
+    id: '2',
+    name: 'Japonca',
+    priority: 3,
+    visibility: true
+  },
+  {
+    id: '3',
+    name: 'Almanca',
+    priority: 2,
+    visibility: true
+  },
+  {
+    id: '4',
+    name: 'Fransızca',
+    priority: 1,
+    visibility: true
+  }
+];
+
+const languageLevels = [
+  {
+    id: '1',
+    name: 'Temel Seviye (A1, A2)',
+    priority: 4,
+    visibility: true
+  },
+  {
+    id: '2',
+    name: 'Orta Seviye (B1, B2)',
+    priority: 3,
+    visibility: true
+  },
+  {
+    id: '3',
+    name: 'İleri Seviye (C1, C2)',
+    priority: 2,
+    visibility: true
+  },
+  {
+    id: '4',
+    name: 'Anadil',
+    priority: 1,
+    visibility: true
+  }
+];
 
 const ForeignLanguageForm = (props: Props) => {
   return (
     <div className="foreing-language">
-      <div className="foreing-language-input">
-        <div className="foreing-language-name-input input-container-w-50">
-          <select name="foreing-language-name">
-            <option value="">Dil Seçiniz*</option>
-            <option value="1">Almanca</option>
-            <option value="2">İngilizce</option>
-            <option value="3">Korece</option>
-            <option value="4">Japonca</option>
-          </select>
-        </div>
-        <div className="foreing-language-level-input input-container-w-50">
-          <select name="foreing-language-level">
-            <option value="">Seviye Seçiniz*</option>
-            <option value="1">Temel Seviye (A1, A2)</option>
-            <option value="2">Orta Seviye (B1, B2)</option>
-            <option value="3">İleri Seviye (C1, C2)</option>
-            <option value="4">Anadil</option>
-          </select>
-        </div>
+      <div className="foreing-language-input-section">
+        <InputContainer
+          inputContainerClasses="foreing-language-name-input-container input-container-w-50"
+          elementType="select"
+          inputName="foreing-language-name"
+          defaultOptionText="Dil Seçiniz*"
+          optionData={languages}
+          optionDataFilters={languagesOptionDataFilters}
+          optionDataSort={sortByPriorityDesc}
+        />
+
+        <InputContainer
+          inputContainerClasses="foreing-language-level-input-container input-container-w-50"
+          elementType="select"
+          inputName="foreing-language-level"
+          defaultOptionText="Seviye Seçiniz*"
+          optionData={languageLevels}
+          optionDataFilters={languageLevelsOptionDataFilters}
+          optionDataSort={sortByPriorityDesc}
+        />
       </div>
       <button type="submit" className="foreing-language-save-button">
         Kaydet
