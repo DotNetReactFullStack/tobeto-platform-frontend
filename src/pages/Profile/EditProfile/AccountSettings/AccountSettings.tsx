c97a701bfda1f9af6e8b5e45d13807b75f8255c3
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-21 15:00:22 +0300

    Implemented responsive design for the account settings page's components

--- src/pages/Profile/EditProfile/AccountSettings/AccountSettings.tsx
+++ src/pages/Profile/EditProfile/AccountSettings/AccountSettings.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import LinkList from "../../../../components/EditProfile/Link/LinkList";
 import AccountSettingsForm from "../../../../components/EditProfile/AccountSettings/AccountSettingsForm";
+import './AccountSettings.css'
 
 type Props = {};
 
 const AccountSettings = (props: Props) => {
   return (
-    <div className="account-settings-page-body container main-section d-flex">
+    <div className="account-settings-page-body container main-section">
       <div className="edit-profile-page-left-side">
         <LinkList />
       </div>
