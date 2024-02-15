d1531b57fc91fcf5fb7ac3bf332eafa4caf0eb0a
OzgurSonmez <ozgursonmez00@gmail.com>
2024-02-15 18:11:31 +0300

    Updated fetchData in CapabilityForm component

--- src/components/EditProfile/Capability/CapabilityForm.tsx
+++ src/components/EditProfile/Capability/CapabilityForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CapabilityForm.css";
 import InputContainer from "../InputContainer";
 import { FormElementType } from "../../../models/formElementType";
@@ -12,9 +12,6 @@ const ifVisibilityIsTrue = (value: any) => value.visibility === true;
 const ifVisibilityIsFalse = (value: any) => value.visibility === false;
 let capabilitiesOptionDataFilters = [ifVisibilityIsTrue];
 const sortByPriorityDesc = (a: any, b: any) => b.priority - a.priority;
-
-const capabilitiesResponse = capabilityService.getAll();
-const capabilities = (await capabilitiesResponse).data.items;
 
 //Formik, Yup
 const initialValues: any = {
@@ -33,6 +30,16 @@ const handleCapability = async (values: any) => {
 };
 
 const CapabilityForm = (props: Props) => {
+  const [capabilities, setCapabilities] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const capabilitiesResponse = await capabilityService.getAll();
+      const capabilitiesData = capabilitiesResponse.data.items;
+      setCapabilities(capabilitiesData);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="capability-form">
       <Formik
