64b95fc4956de61dbd9487a67f3645a50cedd265
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-02-15 15:09:15 +0300

    Fetched capabilities data from the database

---
+++ src/services/capabilityService.ts
@@ -0,0 +1,22 @@
+import { an } from "@fullcalendar/core/internal-common";
+import { BaseService } from "../core/services/baseService";
+import { BASE_API_URL } from "../environment/environment";
+
+class CapabilityService extends BaseService
+    <
+        any,
+        any,
+        any,
+        any,
+        any,
+        any
+    > {
+    constructor() {
+        super();
+        this.apiUrl = BASE_API_URL + "Capabilities?PageIndex=0&PageSize=20000";
+    }
+
+    getByFilter() { }
+}
+
+export default new CapabilityService();
