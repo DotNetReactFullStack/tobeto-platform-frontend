445f188686efd62534422c608d7570c42a8fc52d
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-02-22 20:38:10 +0300

    Updated EducationAndExperienceList component

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -109,82 +109,6 @@ const generateActivityData = () => {
 const activityData = generateActivityData();
 
 const Profile = (props: Props) => {
-
-  const dispatch = useDispatch();
-
-  const currentAccount = useSelector((state: any) => state.account.currentAccount.payload);
-  const accountId = currentAccount.id;
-
-  const accountExperiences: GetListByAccountIdExperienceListItemDto[] = useSelector((state: RootState) => state.experience.accountExperiences);
-
-  const accountCollegeMetadatas: GetListByAccountIdAccountCollegeMetadataListItemDto[] =
-    useSelector(
-      (state: RootState) =>
-        state.accountCollegeMetadata.accountCollegeMetadatas
-    );
-
-  async function fetchAccountExperienceData() {
-    try {
-      const accountExperiencesResponse = await experienceService.getListByAccountId(accountId);
-      const data = accountExperiencesResponse.data.items;
-      dispatch(setAccountExperiences(data));
-    } catch (error) {
-      console.error("Veri alınamadı:", error);
-    }
-  }
-
-  async function fetchAccountCollegeMetadata() {
-    try {
-      const accountCollegeMetadatasResponse =
-        await accountCollegeMetadataService.getListByAccountId(accountId);
-      const data = accountCollegeMetadatasResponse.data.items;
-      dispatch(setAccountCollegeMetadatas(data));
-    } catch (error) {
-      console.error("Veri alınamadı:", error);
-    }
-  }
-
-  const mergeData = (accountExperiences: GetListByAccountIdExperienceListItemDto[], accountCollegeMetadatas: GetListByAccountIdAccountCollegeMetadataListItemDto[]) => {
-    let historyExperienceData: ProfileHistoryElementExperienceModel[] = [];
-    let historyEducationData: ProfileHistoryElementEducationModel[] = [];
-
-    historyExperienceData = accountExperiences.map((experience) => { return { elementType: ProfileHistoryElementType.Experience, experienceData: experience } })
-
-    historyEducationData = accountCollegeMetadatas.map((education) => { return { elementType: ProfileHistoryElementType.Education, educationData: education } })
-
-    return [...historyExperienceData, ...historyEducationData];
-  }
-
-  const educationAndExperienceData = mergeData(accountExperiences, accountCollegeMetadatas);
-
-  const allStartingDates: string[] = [];
-  educationAndExperienceData.forEach((item: any) => {
-    if (item.elementType === 'education') {
-      allStartingDates.push(item.educationData.startingYear);
-    } else {
-      allStartingDates.push(item.experienceData.startingDate);
-    }
-  });
-
-  // Başlangıç tarihlerine göre diziyi karmaşık bir şekilde sıralama
-  educationAndExperienceData.sort((a: any, b: any) => { // refactor
-    const dateA = a.elementType === 'education' ? a.educationData.startingYear : a.experienceData.startingDate;
-    const dateB = b.elementType === 'education' ? b.educationData.startingYear : b.experienceData.startingDate;
-    // Başlangıç tarihlerine göre sıralama
-    if (dateA === dateB) {
-      // Aynı başlangıç tarihine sahipler, elementType'e göre sıralama
-      return a.elementType === 'education' ? -1 : 1;
-    } else {
-      // Başlangıç tarihlerine göre sıralama
-      return new Date(dateB).getTime() - new Date(dateA).getTime();
-    }
-  });
-
-  useEffect(() => {
-    fetchAccountExperienceData();
-    fetchAccountCollegeMetadata();
-  }, []);
-
   return (
     <div className="container main-section d-flex flex-column">
       <div className="profile-page-header">
@@ -225,7 +149,7 @@ const Profile = (props: Props) => {
           </ProfileDefaultCard>
 
           <ProfileDefaultCard title="Eğitim Hayatım ve Deneyimlerim">
-            <EducationAndExperienceList data={educationAndExperienceData} />
+            <EducationAndExperienceList />
           </ProfileDefaultCard>
         </div>
       </div>
